901ea15af570661c8c7a63e53288d57a3ea33b37
Scrapbook Git Tutorial <git-tutorial@joinscrapbook.com>
2020-07-20 19:07:11 +0000

    email function added

--- PythonLearning/class1.py
+++ PythonLearning/class1.py
@@ -4,5 +4,13 @@ class Employee:
 		self.lname = lname
 		self.pay = pay
 
+<<<<<<< HEAD
+=======
+	def fullname(self):
+		return '{}  {}'.format(self.fname, self.lname)
+	
+	def email(self):
+		return '{}.{}@company.com'.format(self.fname,self.lname)
+>>>>>>> fc9ba61... email funtion added
 
 
