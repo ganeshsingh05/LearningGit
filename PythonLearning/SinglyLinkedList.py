c1775944193f797701a367b5796462a77e90bef2
Ganesh Singh <ganeshbisht1@gmail.com>
2021-01-07 12:43:00 +0530

    Class Node and LinkedList are created

---
+++ PythonLearning/SinglyLinkedList.py
@@ -0,0 +1,11 @@
+# Node Class
+class Node:
+    # Function to initialize the node object 
+    def __init__(self, data): 
+        self.data = data  # Assign data 
+        self.next = None  # Initialize next as null 
+  
+# Linked List class 
+class LinkedList:
+    def __init__(self):
+        self.head = None
