df19f55cab18f253dac0d759f3408b4b9b933367
Ganesh Singh <ganeshbisht1@gmail.com>
2021-01-07 13:07:16 +0530

    insert at end and print function added

--- PythonLearning/SinglyLinkedList.py
+++ PythonLearning/SinglyLinkedList.py
@@ -9,3 +9,35 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def insertEnd(self, newNode):
+        if self.head is None:
+            self.head = newNode
+        else:
+            lastNode = self.head
+            while True:
+                if lastNode.next is None:
+                    break
+                lastNode = lastNode.next
+            lastNode.next = newNode
+
+    def printList(self):
+        currentNode = self.head
+        while True:
+            if currentNode is None:
+                return('Empty linkedList')
+            print(currentNode.data)
+            currentNode = currentNode.next
+
+if __name__ == '__main__':
+
+    firstNode = Node('Ganesh')
+    linkedList = LinkedList()
+    linkedList.insertEnd(firstNode)
+    SecondNode = Node('Singh')
+    linkedList.insertEnd(SecondNode)
+    thirdNode = Node('TCS')
+    linkedList.insertEnd(thirdNode)
+    linkedList.printList()
+
+
